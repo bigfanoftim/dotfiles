c73d3fc22340ea5e2a99cf6265d061bc68ef23b5
bigfanoftim <wkdghwns91@gmail.com>
2022-08-24 22:13:11 +0900

    Fixed indent

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -1,57 +1,62 @@
 return require('packer').startup(function()
 	-- packer can manage itself
-	use 'wbthomason/packer.nvim'
+  use 'wbthomason/packer.nvim'
 
 	-- colorscheme
-	use 'morhetz/gruvbox'
-	use 'ayu-theme/ayu-vim'
+  use 'morhetz/gruvbox'
+  use 'ayu-theme/ayu-vim'
 
 	-- Syntax
   use 'jparise/vim-graphql'
   use 'pangloss/vim-javascript'
   use 'vim-python/python-syntax'
-	use 'Vimjas/vim-python-pep8-indent'
-	use 'nathanaelkane/vim-indent-guides'
-	use 'nanotee/sqls.nvim'
+  use 'Vimjas/vim-python-pep8-indent'
+  use 'nathanaelkane/vim-indent-guides'
+  use 'nanotee/sqls.nvim'
 
 	-- Neovim
   use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
-	}
-	use {
-		'neoclide/coc.nvim', 
-		branch = 'release'
-	}
-	use 'neovim/nvim-lspconfig'
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use {
+    'neoclide/coc.nvim', 
+    branch = 'release'
+  }
+  use {
+      "williamboman/nvim-lsp-installer",
+      "neovim/nvim-lspconfig",
+  }
 
+  -- NerdTree
+  use 'preservim/nerdtree'
 
-	-- NerdTree
-	use 'preservim/nerdtree'
+  -- junegunn
+  use 'junegunn/vim-easy-align'
+  use {
+    'junegunn/fzf',
+    run = "fzf#install()"
+  }
+  use 'junegunn/fzf.vim'
 
-	-- junegunn
-	use 'junegunn/vim-easy-align'
-	use {
-		'junegunn/fzf',
-		run = "fzf#install()"
-	}
-	use 'junegunn/fzf.vim'
+  -- tpope
+  use 'tpope/vim-fugitive'
 
-	-- vim-airline
-	use 'vim-airline/vim-airline'
-	use 'vim-airline/vim-airline-themes'
+  -- vim-airline
+  use 'vim-airline/vim-airline'
+  use 'vim-airline/vim-airline-themes'
 
-	-- more
-	use 'jiangmiao/auto-pairs'
-	use 'kassio/neoterm'
-	use {'mg979/vim-visual-multi', branch = 'master'}
-	use 'tpope/vim-commentary'
-	use ({ 
-		"iamcco/markdown-preview.nvim", 
-		run = "cd app && npm install", 
-		setup = function() vim.g.mkdp_filetypes = { "markdown" } end, 
-		ft = { "markdown" }, 
-	})
+  -- more
+  use 'jiangmiao/auto-pairs'
+  use 'kassio/neoterm'
+  use {'mg979/vim-visual-multi', branch = 'master'}
+  use 'tpope/vim-commentary'
+  use ({ 
+    "iamcco/markdown-preview.nvim", 
+    run = "cd app && npm install", 
+    setup = function() vim.g.mkdp_filetypes = { "markdown" } end, 
+    ft = { "markdown" }, 
+  })
 
 end)
 
