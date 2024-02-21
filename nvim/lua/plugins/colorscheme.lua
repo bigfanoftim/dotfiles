0b80594535177ac5f545cbe3bd6cbc34e4ab8a4d
bigfanoftim <bigfanoftim@gmail.com>
2024-02-22 08:44:56 +0900

    feat(nvim): gruvbox-material colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,17 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    return {
-      transparent = true,
-    }
-  end,
+  -- "craftzdog/solarized-osaka.nvim", # devaslife, takuya's solarized theme config
+  -- lazy = true,
+  -- priority = 1000,
+  -- opts = function()
+  --   return {
+  --     transparent = true,
+  --   }
+  -- end,
+  { "wittyjudge/gruvbox-material.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
 }
