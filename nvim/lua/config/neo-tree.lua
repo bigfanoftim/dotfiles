2a98a6c79df33ed2fcfd5a4f289e3ec855a3b215
bigfanoftim <bigfanoftim@gmail.com>
2024-02-22 17:47:42 +0900

    fix(nvim): show hidden, ignored files

---
+++ nvim/lua/config/neo-tree.lua
@@ -0,0 +1,10 @@
+require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      visible = false, -- when true, they will just be displayed differently than normal items
+      hide_dotfiles = false,
+      hide_gitignored = false,
+      hide_hidden = false, -- only works on Windows for hidden files/directories
+    },
+  },
+})
