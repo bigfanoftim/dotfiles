9af527fac93b79f0f627f521f7aff089a27afda1
bigfanoftim <bigfanoftim@gmail.com>
2024-02-22 18:49:08 +0900

    fix(nvim): unmap move up&down

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -11,9 +11,6 @@ keymap.set("n", "-", "<C-x>")
 -- Delete a word backwards
 keymap.set("n", "dw", "vb_d")
 
--- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G")
-
 -- Jumplist
 keymap.set("n", "<C-m>", "<C-i>", opts)
 
@@ -25,11 +22,6 @@ keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 -- Split window
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
--- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
 
 -- Resize window
 keymap.set("n", "<C-w><left>", "<C-w><")
@@ -37,7 +29,10 @@ keymap.set("n", "<C-w><right>", "<C-w>>")
 keymap.set("n", "<C-w><up>", "<C-w>+")
 keymap.set("n", "<C-w><down>", "<C-w>-")
 
--- Diagnostics
-keymap.set("n", "<C-j>", function()
-  vim.diagnostics.goto_next()
-end, opts)
+-- fix move up & down
+vim.keymap.del("n", "<A-j>")
+vim.keymap.del("n", "<A-k>")
+vim.keymap.del("i", "<A-j>")
+vim.keymap.del("i", "<A-k>")
+vim.keymap.del("v", "<A-j>")
+vim.keymap.del("v", "<A-k>")
