752ea762881b2e57afe4af2a295b55846a87a75d
bigfanoftim <bigfanoftim@gmail.com>
2024-02-23 23:15:13 +0900

    feat(nvim): import markdown extras module
    Deactivated concealling on markdown file type.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -16,3 +16,11 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.wo.conceallevel = 0
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown" },
+  callback = function()
+    vim.wo.spell = false
+    vim.wo.conceallevel = 0
+  end,
+})
