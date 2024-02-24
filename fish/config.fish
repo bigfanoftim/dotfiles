a8831b6dbd0ee4c5355414d8d006b26a1f43a5a3
bigfanoftim <bigfanoftim@gmail.com>
2024-02-24 18:27:44 +0900

    feat(fish): new fish shell conf with startship

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias vi="nvim"
+
+starship init fish | source
+
+set PATH /usr/local/bin /usr/sbin /opt/homebrew/bin /opt/homebrew/sbin /Users/bigfanoftim/Library/maven/apache-maven-3.6.3/bin /Users/bigfanoftim/.sdkman/candidates/java/current/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin /Users/bigfanoftim/.fig/bin /Users/bigfanoftim/.local/bin /Users/bigfanoftim/Library/Application Support/JetBrains/Toolbox/scripts $PATH
