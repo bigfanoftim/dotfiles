1c0fe91e949bb9e0b212097ba22cdace759640fd
bigfanoftim <bigfanoftim@gmail.com>
2024-03-26 11:46:23 +0900

    feat(fish): export locale envs

--- fish/config.fish
+++ fish/config.fish
@@ -6,4 +6,7 @@ alias vi="nvim"
 
 starship init fish | source
 
+export LC_ALL=ko_KR.UTF-8
+export LANG=ko_KR.UTF-8
+
 set PATH /usr/local/bin /usr/sbin /opt/homebrew/bin /opt/homebrew/sbin /Users/bigfanoftim/Library/maven/apache-maven-3.6.3/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin /Users/bigfanoftim/.fig/bin /Users/bigfanoftim/.local/bin /Users/bigfanoftim/Library/Application Support/JetBrains/Toolbox/scripts $PATH
