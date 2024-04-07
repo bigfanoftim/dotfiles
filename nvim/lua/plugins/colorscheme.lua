400a249b937e6563ab0bbdf894069f31ca268330
bigfanoftim <bigfanoftim@gmail.com>
2024-04-07 20:48:39 +0900

    refactor(nvim): require statement to colorscheme configuration, set conceallevel to 0

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,26 @@
+local function configure_gruvbox()
+  require("gruvbox").setup({
+    terminal_colors = true,
+    undercurl = true,
+    bold = true,
+    italic = {
+      strings = true,
+      emphasis = true,
+      comments = true,
+      operators = false,
+      folds = true,
+    },
+    contrast = "soft",
+    transparent = true,
+  })
+  vim.cmd([[colorscheme gruvbox]])
+end
+
 return {
   {
     "ellisonleao/gruvbox.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme gruvbox]])
-    end,
+    config = configure_gruvbox,
   },
 }
