9be96a0b417e88aa0a96ed7301dc07efe9c16232
bigfanoftim <bigfanoftim@gmail.com>
2024-04-09 21:04:26 +0900

    feat(nvim): custom lazygit configs

--- nvim/lua/plugins/lazy-git.lua
+++ nvim/lua/plugins/lazy-git.lua
@@ -1,12 +1,18 @@
--- brew install lazygit
 return {
-  { "kdheepak/lazygit.nvim" },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-      },
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
     },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+    config = function()
+      vim.g.lazygit_floating_window_winblend = 20
+      vim.g.lazygit_floating_window_scaling_factor = 0.9
+      vim.g.lazygit_floating_window_border_chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+      vim.g.lazygit_floating_window_use_plenary = 0
+      vim.g.lazygit_use_neovim_remote = 0
+    end,
   },
 }
