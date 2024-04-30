4ae492a7596a640b792e2e01cfb2ed1cfd403684
bigfanoftim <bigfanoftim@gmail.com>
2024-04-30 22:58:06 +0900

    Add copilot.vim plugin and set scrolloff to 999

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -2,7 +2,8 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
@@ -15,6 +16,9 @@ require("lazy").setup({
       opts = {
         -- colorscheme = "solarized-osaka",
       },
+    },
+    {
+      "github/copilot.vim"
     },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.linting.eslint" },
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,7 @@
 local opt = vim.opt
 
 opt.conceallevel = 0
+opt.scrolloff = 999
 
 -- Always set the directory at the time of neovim entry to root dir.
 vim.g.root_spec = { "cwd" }
