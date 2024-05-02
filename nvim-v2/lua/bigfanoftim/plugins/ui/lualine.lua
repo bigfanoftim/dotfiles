d1e1736965e79bfb8141032f9189bdeeab4da709
bigfanoftim <bigfanoftim@gmail.com>
2024-05-02 23:03:29 +0900

    Display directory path

--- nvim-v2/lua/bigfanoftim/plugins/ui/lualine.lua
+++ nvim-v2/lua/bigfanoftim/plugins/ui/lualine.lua
@@ -1,45 +1,49 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.g.lualine_laststatus = vim.o.laststatus
-		if vim.fn.argc(-1) > 0 then
-			-- set an empty statusline till lualine loads
-			vim.o.statusline = " "
-		else
-			-- hide the statusline on the starter page
-			vim.o.laststatus = 0
-		end
-	end,
-	opts = function()
-		-- PERF: we don't need this lualine require madness 🤷
-		local lualine_require = require("lualine_require")
-		lualine_require.require = require
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.g.lualine_laststatus = vim.o.laststatus
+    if vim.fn.argc(-1) > 0 then
+      -- set an empty statusline till lualine loads
+      vim.o.statusline = " "
+    else
+      -- hide the statusline on the starter page
+      vim.o.laststatus = 0
+    end
+  end,
+  opts = function()
+    -- PERF: we don't need this lualine require madness 🤷
+    local lualine_require = require("lualine_require")
+    lualine_require.require = require
 
-		-- local icons = require("lazyvim.config").icons
+    vim.o.laststatus = vim.g.lualine_laststatus
 
-		vim.o.laststatus = vim.g.lualine_laststatus
-
-		return {
-			options = {
-				theme = "auto",
-				globalstatus = true,
-				disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch" },
-				lualine_y = {
-					{ "progress", separator = " ", padding = { left = 1, right = 0 } },
-					{ "location", padding = { left = 0, right = 1 } },
-				},
-				lualine_z = {
-					function()
-						return "  " .. os.date("%R")
-					end,
-				},
-			},
-			extensions = { "neo-tree", "lazy" },
-		}
-	end,
+    return {
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_y = {
+          { "progress", separator = " ",                  padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+          {
+            function()
+              return vim.fn.fnamemodify(vim.fn.expand("%:p"), ":~:.")
+            end,
+            icon = "📁",
+          },
+        },
+        lualine_z = {
+          function()
+            return "  " .. os.date("%R")
+          end,
+        },
+      },
+      extensions = { "neo-tree", "lazy" },
+    }
+  end,
 }
