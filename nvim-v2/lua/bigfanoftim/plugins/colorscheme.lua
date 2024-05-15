05d4c7a857c2dcb0ec8c69636c0167e4612fb1be
bigfanoftim <bigfanoftim@gmail.com>
2024-05-15 23:30:35 +0900

    Add stylua.toml for lua formatting

--- nvim-v2/lua/bigfanoftim/plugins/colorscheme.lua
+++ nvim-v2/lua/bigfanoftim/plugins/colorscheme.lua
@@ -1,10 +1,7 @@
 return {
-	"wittyjudge/gruvbox-material.nvim",
-	config = function()
-		require("gruvbox-material").setup({
-
-		})
-		vim.cmd("colorscheme gruvbox-material")
-	end
+  "wittyjudge/gruvbox-material.nvim",
+  config = function()
+    require("gruvbox-material").setup({})
+    vim.cmd("colorscheme gruvbox-material")
+  end,
 }
-
--- nvim-v2/lua/bigfanoftim/plugins/conform.lua
+++ nvim-v2/lua/bigfanoftim/plugins/conform.lua
@@ -1,20 +1,20 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {},
+  "stevearc/conform.nvim",
+  opts = {},
 
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "prettier" },
-			},
-			-- format_on_save = {
-			--   lsp_fallback = true,
-			--   timeout = 100,
-			-- },
-			format_after_save = { -- Conform will run the formatter asynchronously after saving the file
-				lsp_fallback = true,
-			},
-		})
-	end,
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { "prettier" },
+      },
+      -- format_on_save = {
+      --   lsp_fallback = true,
+      --   timeout = 100,
+      -- },
+      format_after_save = { -- Conform will run the formatter asynchronously after saving the file
+        lsp_fallback = true,
+      },
+    })
+  end,
 }
