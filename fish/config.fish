4418a00b6a4df186647ab2a73808e527103af7ba
bigfanoftim <bigfanoftim@gmail.com>
2024-07-05 11:11:22 +0900

    New lazydocker ssh alias in config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,9 +5,16 @@ end
 alias vi="nvim"
 
 alias lzd="set -e DOCKER_HOST && lazydocker"
-alias lzd-dev="ssh -L ~/docker-remote.sock:/var/run/docker.sock ds-dev -fN && export DOCKER_HOST=unix:///Users/bigfanoftim/docker-remote.sock && sudo chmod 666 ~/docker-remote.sock && lazydocker"
+alias lzd-dev="ssh -L ~/docker-remote-dev.sock:/var/run/docker.sock vd-dev-containers -fN && export DOCKER_HOST=unix:///Users/bigfanoftim/docker-remote-dev.sock && sudo chmod 666 ~/docker-remote-dev.sock && lazydocker"
 alias lzd-stage="ssh -L ~/docker-remote-stage.sock:/var/run/docker.sock ds-stage -fN && export DOCKER_HOST=unix:///Users/bigfanoftim/docker-remote-stage.sock && sudo chmod 666 ~/docker-remote-stage.sock && lazydocker"
-alias lzd-prod="ssh -L ~/docker-remote-prod.sock:/var/run/docker.sock ds-prod -fN && export DOCKER_HOST=unix:///Users/bigfanoftim/docker-remote-prod.sock && sudo chmod 666 ~/docker-remote-prod.sock && lazydocker"
+alias lzd-prod="ssh -L ~/docker-remote-prod.sock:/var/run/docker.sock vd-prd-admin -fN && export DOCKER_HOST=unix:///Users/bigfanoftim/docker-remote-prod.sock && sudo chmod 666 ~/docker-remote-prod.sock && lazydocker"
+alias lzd-global="ssh -L ~/docker-remote-global.sock:/var/run/docker.sock global-maintenance -fN && export DOCKER_HOST=unix:///Users/bigfanoftim/docker-remote-global.sock && sudo chmod 666 ~/docker-remote-global.sock && lazydocker"
+
+alias lzd-ksnet-dev="ssh -L ~/docker-remote-ksnet-dev.sock:/var/run/docker.sock ksnet-dev -fN && export DOCKER_HOST=unix:///Users/bigfanoftim/docker-remote-ksnet-dev.sock && sudo chmod 666 ~/docker-remote-ksnet-dev.sock && lazydocker"
+alias lzd-ksnet-staging="ssh -L ~/docker-remote-ksnet-staging.sock:/var/run/docker.sock ksnet-staging -fN && export DOCKER_HOST=unix:///Users/bigfanoftim/docker-remote-ksnet-staging.sock && sudo chmod 666 ~/docker-remote-ksnet-staging.sock && lazydocker"
+alias lzd-ksnet-prod-containers-1="ssh -L ~/docker-remote-ksnet-prod-containers-1.sock:/var/run/docker.sock ksnet-prod-containers-1 -fN && export DOCKER_HOST=unix:///Users/bigfanoftim/docker-remote-ksnet-prod-containers-1.sock && sudo chmod 666 ~/docker-remote-ksnet-prod-containers-1.sock && lazydocker"
+alias lzd-ksnet-prod-containers-2="ssh -L ~/docker-remote-ksnet-prod-containers-2.sock:/var/run/docker.sock ksnet-prod-containers-2 -fN && export DOCKER_HOST=unix:///Users/bigfanoftim/docker-remote-ksnet-prod-containers-2.sock && sudo chmod 666 ~/docker-remote-ksnet-prod-containers-2.sock && lazydocker"
+alias lzd-ksnet-prod-gateway="ssh -L ~/docker-remote-ksnet-prod-gateway.sock:/var/run/docker.sock ksnet-prod-gateway -fN && export DOCKER_HOST=unix:///Users/bigfanoftim/docker-remote-ksnet-prod-gateway.sock && sudo chmod 666 ~/docker-remote-ksnet-prod-gateway.sock && lazydocker"
 
 alias lzd-feedoong="ssh -L ~/docker-remote.sock:/var/run/docker.sock feedoong -fN && export DOCKER_HOST=unix:///Users/bigfanoftim/docker-remote.sock && sudo chmod 666 ~/docker-remote.sock && lazydocker"
 
@@ -20,6 +27,7 @@ alias obk="vi $HOME/dev/obsidian/obsidian-nvim-with-quartz/content/kanban/vd.md"
 # alias obu="cd $HOME/notion-obsidian-sync && node batchUpload.js --lastmod-days-window 5"
 
 alias vdtf="cd $HOME/dev/vdcrux/infrastructure/terraform"
+alias vdtg="cd $HOME/dev/vdcrux/infrastructure/terraform/terragrunt/vd/ap-northeast-2"
 alias vdn="cd $HOME/dev/vdcrux/vd_company"
 alias vdo="cd $HOME/dev/vdcrux/vdcompany"
 
